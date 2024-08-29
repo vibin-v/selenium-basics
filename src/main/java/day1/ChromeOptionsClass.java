fb3f367a068578ff350fe9a8704e1c74bc36d228
vibi <vibi.vijayakumar@gmail.com>
2024-08-29 19:47:55 +0530

    Element locators updated on options class

--- src/main/java/day1/ChromeOptionsClass.java
+++ src/main/java/day1/ChromeOptionsClass.java
@@ -1,6 +1,8 @@
 package day1;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
@@ -11,6 +13,30 @@ public class ChromeOptionsClass {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--start-maximized");
         WebDriver driver = new ChromeDriver(options);
+
+        driver.get("https://www.facebook.com/");
+
+        WebElement printLink = driver.findElement(By.linkText("Forgotten password?"));
+        String linkTxt = printLink.getText();
+        System.out.println(linkTxt);
+
+        /*
+    
+        driver.findElement(By.xpath("//input[contains(@id,'pass')]")).sendKeys("L");
+        driver.findElement(By.xpath("//a[text()='Forgotten password?']"));
+        driver.findElement(By.xpath("//a[contains(text(),'Forgotten password?')]"));
+
+
+        driver.findElement(By.xpath("//input[@id='email']")).sendKeys("H");
+        driver.findElement(By.id("email")).sendKeys("E");
+        driver.findElement(By.linkText("Forgotten password?"));
+
+        driver.findElement(By.xpath("//input[@id='email']")).sendKeys("H");
+        driver.findElement(By.id("email")).sendKeys("E");
+
+        driver.findElement(By.cssSelector("#reg_pages_msg>a")).click();
+     
+         */
     }
     
 }
