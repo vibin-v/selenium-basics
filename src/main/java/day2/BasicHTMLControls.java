2968350ca4cc572d718443f4863e8f88ee721869
vibi <vibi.vijayakumar@gmail.com>
2024-08-30 14:06:27 +0530

    Remved unused imports from Basichtmlcontrols

--- src/main/java/day2/BasicHTMLControls.java
+++ src/main/java/day2/BasicHTMLControls.java
@@ -1,14 +1,10 @@
 package day2;
 
-import java.util.List;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.testng.collections.Lists;
-import org.testng.util.Strings;
 
 public class BasicHTMLControls {
     public static void main(String[] args) {
